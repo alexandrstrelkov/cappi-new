d4ed2369efb2af618d6fc5082728ca999614e4bb
alexandrstrelkov <32618502+alexandrstrelkov@users.noreply.github.com>
2025-05-21 18:28:00 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+import LandingPage from './pages/LandingPage';
+import AppPage from './pages/AppPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/app" element={<AppPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
