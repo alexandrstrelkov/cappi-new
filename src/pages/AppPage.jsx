ebc55465fdd60fe67519202b14149b6911190045
alexandrstrelkov <32618502+alexandrstrelkov@users.noreply.github.com>
2025-05-21 18:54:09 +0300

    Update AppPage.jsx

--- src/pages/AppPage.jsx
+++ src/pages/AppPage.jsx
@@ -1,1 +1,13 @@
+// src/pages/AppPage.jsx
+import React from 'react';
 
+function AppPage() {
+  return (
+    <div>
+      {/* Ваш код */}
+      <h1>App Page</h1>
+    </div>
+  );
+}
+
+export default AppPage;
