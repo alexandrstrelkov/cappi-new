f0029845a04200fe3041f9d946a1d04d7dfe16a3
alexandrstrelkov <32618502+alexandrstrelkov@users.noreply.github.com>
2025-05-21 18:35:09 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,16 @@
-
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        cappiPurple: "#7e22ce", // optional custom name
+        cappiPink: "#ec4899"
+      }
+    },
+  },
+  plugins: [],
+}
